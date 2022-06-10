bfe866bb3d384919edc8b5ae747b35fbfa3682d1
huubl <>
2022-06-10 13:45:12 +0200

    Update deps + h2 > h3

--- blocks/accordion/build/index.asset.php
+++ blocks/accordion/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'f9236eb4863144cf6c5fdd9f2724f346');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '5ddd89df0e81b0527fb4');
