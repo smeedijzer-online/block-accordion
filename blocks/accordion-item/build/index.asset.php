ce8ac8d6c299415be05f4f4e3085d0418803fed4
huubl <>
2022-08-22 15:41:31 +0200

    Update

--- blocks/accordion-item/build/index.asset.php
+++ blocks/accordion-item/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '0b3ebb3ed27341931e70');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '6e02bc7cf6562cef935b');
